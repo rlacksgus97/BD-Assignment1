fd0421dc0e027775a24c5d5123a62a5f5042e8d3
rlacksgus97 <rlacksgus97@naver.com>
2022-04-08 10:25:42 +0900

    [feat] : 회원가입, 로그인, jwt 인증

---
+++ src/main/java/com/bd/assignment1/user/UserController.java
@@ -0,0 +1,34 @@
+package com.bd.assignment1.user;
+
+import com.bd.assignment1.user.dto.JoinReqDto;
+import com.bd.assignment1.user.dto.JoinResDto;
+import com.bd.assignment1.user.dto.LoginReqDto;
+import com.bd.assignment1.user.dto.LoginResDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public ResponseEntity<JoinResDto> join(@RequestBody JoinReqDto joinDto) throws Exception {
+        return ResponseEntity.ok(userService.join(joinDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResDto> login(@RequestBody LoginReqDto loginDto) throws Exception {
+        return ResponseEntity.ok(userService.login(loginDto));
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<LoginResDto> test() {
+        return ResponseEntity.ok(null);
+    }
+}
---
+++ src/main/java/com/bd/assignment1/user/dto/LoginReqDto.java
@@ -0,0 +1,9 @@
+package com.bd.assignment1.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginReqDto {
+    private String email;
+    private String password;
+}
