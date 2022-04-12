8554a0001546b516e45d25ec9662fea01999c5c5
rlacksgus97 <rlacksgus97@naver.com>
2022-04-12 16:53:28 +0900

    [feat] : 댓글, 대댓글 페이징 처리

--- src/main/java/com/bd/assignment1/comment/CommentController.java
+++ src/main/java/com/bd/assignment1/comment/CommentController.java
@@ -1,9 +1,11 @@
 package com.bd.assignment1.comment;
 
+import com.bd.assignment1.comment.dto.ChildCommentResDto;
 import com.bd.assignment1.comment.dto.ParentCommentResDto;
 import com.bd.assignment1.comment.dto.CreateCommentReqDto;
 import com.bd.assignment1.comment.dto.UpdateCommentReqDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -24,20 +26,23 @@ public class CommentController {
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<List<ParentCommentResDto>> list(@PathVariable Long postId) {
-        return ResponseEntity.ok(commentService.list(postId));
+    public ResponseEntity<List<ParentCommentResDto>> getParentList(@PathVariable Long postId, Pageable pageable) {
+        return ResponseEntity.ok(commentService.getParentList(postId, pageable));
+    }
+
+    @GetMapping("/{postId}/{commentId}")
+    public ResponseEntity<List<ChildCommentResDto>> getChildrenList(@PathVariable Long commentId, Pageable pageable) {
+        return ResponseEntity.ok(commentService.getChildrenList(commentId, pageable));
     }
 
     @PostMapping("/{postId}/{commentId}")
-    public ResponseEntity<Long> update(@PathVariable Long postId,
-                                       @PathVariable Long commentId,
+    public ResponseEntity<Long> update(@PathVariable Long commentId,
                                        @RequestBody UpdateCommentReqDto updateCommentReqDto) {
-        return ResponseEntity.ok(commentService.update(postId, commentId, updateCommentReqDto));
+        return ResponseEntity.ok(commentService.update(commentId, updateCommentReqDto));
     }
 
     @DeleteMapping("/{postId}/{commentId}")
-    public ResponseEntity<Long> delete(@PathVariable Long postId,
-                                       @PathVariable Long commentId) {
-        return ResponseEntity.ok(commentService.delete(postId, commentId));
+    public ResponseEntity<Long> delete(@PathVariable Long commentId) {
+        return ResponseEntity.ok(commentService.delete(commentId));
     }
 }
--- src/main/java/com/bd/assignment1/comment/CommentService.java
+++ src/main/java/com/bd/assignment1/comment/CommentService.java
@@ -9,6 +9,8 @@ import com.bd.assignment1.post.Post;
 import com.bd.assignment1.post.PostRepository;
 import com.bd.assignment1.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -29,7 +31,6 @@ public class CommentService {
 
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다."));
-
         Comment comment = Comment.builder()
                 .content(createCommentReqDto.getContent())
                 .build();
@@ -40,7 +41,7 @@ public class CommentService {
         if (createCommentReqDto.getParentId() != 0) {
             Comment parent = commentRepository.findById(createCommentReqDto.getParentId())
                     .orElseThrow(() -> new RuntimeException("존재하지 않는 부모 댓글입니다."));
-            if (parent.getParent().getId() > 0) {
+            if (parent.getParent() != null && parent.getParent().getId() > 0) {
                 throw new RuntimeException("대댓글은 1개까지만 허용됩니다.");
             }
             parent.addChild(comment);
@@ -49,19 +50,13 @@ public class CommentService {
         return commentRepository.save(comment).getId();
     }
 
+    //댓글 5개만, 각 댓글 당 대댓글도 5개만
     @Transactional
-    public List<ParentCommentResDto> list(Long postId) {
+    public List<ParentCommentResDto> getParentList(Long postId, Pageable pageable) {
         List<ParentCommentResDto> parentCommentResDtos = new ArrayList<>();
-        List<Comment> comments = commentRepository.findParentsByPostId(postId);
+        List<Comment> comments = commentRepository.findByPostIdAndParentId(postId, null, pageable);
         for (Comment p : comments) {
-            List<ChildCommentResDto> childCommentResDtos = new ArrayList<>();
-            List<Comment> children = p.getChildren();
-            for (Comment c : children) {
-                childCommentResDtos.add(ChildCommentResDto.builder()
-                        .user(c.getUser().getEmail())
-                        .content(c.getContent())
-                        .build());
-            }
+            List<ChildCommentResDto> childCommentResDtos = this.getChildrenList(p.getId(), PageRequest.of(0, 5));
             parentCommentResDtos.add(ParentCommentResDto.builder()
                     .user(p.getUser().getEmail())
                     .content(p.getContent())
@@ -73,7 +68,20 @@ public class CommentService {
     }
 
     @Transactional
-    public Long update(Long postId, Long commentId, UpdateCommentReqDto updateCommentReqDto) {
+    public List<ChildCommentResDto> getChildrenList(Long parentId, Pageable pageable) {
+        List<ChildCommentResDto> childCommentResDtos = new ArrayList<>();
+        List<Comment> children = commentRepository.findByParentId(parentId, pageable);
+        for (Comment c : children) {
+            childCommentResDtos.add(ChildCommentResDto.builder()
+                    .user(c.getUser().getEmail())
+                    .content(c.getContent())
+                    .build());
+        }
+        return childCommentResDtos;
+    }
+
+    @Transactional
+    public Long update(Long commentId, UpdateCommentReqDto updateCommentReqDto) {
         User user = jwtService.getUserFromJwt();
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 댓글입니다."));
@@ -86,7 +94,7 @@ public class CommentService {
     }
 
     @Transactional
-    public Long delete(Long postId, Long commentId) {
+    public Long delete(Long commentId) {
         User user = jwtService.getUserFromJwt();
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 댓글입니다."));
